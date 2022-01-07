2d5ac568c931b0c3cd82405c996dc1967cbacdb3
moutew <moutew@free.fr>
2022-01-07 10:51:16 +0100

    fix router

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,53 +1,40 @@
 import './index.scss';
 import React from 'react';
-import logo from './Loxya.svg';
-import { Link, Route, useParams, useRouteMatch, BrowserRouter, Redirect } from "react-router-dom";
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import LoginPage from '../../Pages/LoginPage';
 import FullScreenLayout from '../Layouts/FullScreenLayout';
-import NavLeftLayout from '../Layouts/NavLeftLayout';
 import SidebarLayout from '../Layouts/SidebarLayout';
-import AdminPage from '../../Pages/AdminPage';
 import PrivateRoute from '../../PrivateRoute';
 
 import Category from '../../Category';
-import FlexTuto from '../../Components/FlexTuto';
 import Products from '../../Products';
 
 // https://v5.reactrouter.com/web/example/animated-transitions
 
 const App = (): JSX.Element => {
-    let logged = true;
+    const logged = false;
 
     return (
         <BrowserRouter>
 
-            {/* <Route exact path="/">
-                {logged ? <Redirect to="/admin" /> : <Redirect to="/login" /> }
-            </Route> */}
-
-            {/* <FullScreenLayout>
-                <Route path="/login">
+            <Route path="/login">
+                <FullScreenLayout>
                     <LoginPage />
-                </Route>
-            </FullScreenLayout> */}
+                </FullScreenLayout>
+            </Route>
 
+            <PrivateRoute path="/" isAuthenticated={logged} authenticationPath="/login">
+                <SidebarLayout>
+                    <Route path="/admin/categories">
+                        <Category />
+                    </Route>
 
-            <SidebarLayout>
-                <Route path="/admin/categories">
-                    <Category />
-                </Route>
-
-                <Route path="/admin/products">
-                    <Products />
-                </Route>
-            </SidebarLayout>
-
-
-{/*
-            <PrivateRoute path="/admin" isAuthenticated={logged} authenticationPath="/login">
-                <AdminPage />
-            </PrivateRoute> */}
+                    <Route path="/admin/products">
+                        <Products />
+                    </Route>
+                </SidebarLayout>
+            </PrivateRoute>
 
         </BrowserRouter>
     );
